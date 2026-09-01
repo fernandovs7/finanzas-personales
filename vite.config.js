ce45b39009d1ccfb2f5a0c109d4947aefca0bdb3
Fernando Vargas S. <fernandovs7@live.com>
2026-08-31 20:16:30 -0600

    Revert "Add Sites deployment adapter"
    This reverts commit 7045ff39825ca2ededb8e0d3bcd84ff9d68874b0.

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { sites } from "@openai/sites-vite-plugin";
 
 export default defineConfig({
   base: process.env.VITE_BASE_PATH || "./",
-  plugins: [react(), sites()],
+  plugins: [react()],
   build: {
     outDir: "dist",
     emptyOutDir: true,
